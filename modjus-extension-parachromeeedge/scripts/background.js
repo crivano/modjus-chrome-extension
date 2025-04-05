0784bffbcdbc641e55683e90668560e2b3d032f7
João Luis <joaoluis@trf2.jus.br>
2025-04-05 15:56:41 -0300

    Adiciona chave de extensão no manifest e corrige referência de API para uso do namespace chrome

--- modjus-extension-parachromeeedge/scripts/background.js
+++ modjus-extension-parachromeeedge/scripts/background.js
@@ -1,4 +1,4 @@
 // Exemplo de código do background.js
-browser.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
   console.log('A extensão foi instalada!');
 });
